678e0524d9b77e3acfc8a84d2f709f5493ecc6f3
dbqudgns <dbqudgns4632@gmail.com>
2025-01-30 17:15:18 +0900

    inital setting

---
+++ src/main/java/com/culture/BAEUNDAY/BaeundayApplication.java
@@ -0,0 +1,13 @@
+package com.culture.BAEUNDAY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeundayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeundayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/culture/BAEUNDAY/BaeundayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culture.BAEUNDAY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeundayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
