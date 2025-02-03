83f754c3323eb58e9a15c6fa12b77524c8114627
pigglehyun <sarah0661@pusan.ac.kr>
2025-02-03 17:07:05 +0900

    feat: get Post by Id

--- src/main/java/com/culture/BAEUNDAY/domain/post/PostRestController.java
+++ src/main/java/com/culture/BAEUNDAY/domain/post/PostRestController.java
@@ -6,10 +6,7 @@ import com.culture.BAEUNDAY.utils.ApiUtils;
 import com.culture.BAEUNDAY.utils.PageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/posts")
@@ -24,10 +21,17 @@ public class PostRestController {
                                          @RequestParam(value = "status") String status,
                                          @RequestParam(value = "fee") String fee,
                                          @RequestParam(value = "cursor", required = false) String cursor,
-                                         @RequestParam(value = "cursorID", required = false) Long cursorId ) {
+                                         @RequestParam(value = "cursorId", required = false) Long cursorId ) {
 
         PageResponse<?, PostResponse.FindAllDTO> responseDTO = postService.findAll(sort,status,fee,cursor,cursorId);
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
+    }
+
+    @GetMapping("/{postId}")
+    public  ResponseEntity<?> findPostById(@PathVariable(name = "postId") Long postId ){
+        PageResponse<?, PostResponse.FindByIdDTO> responseDTO = postService.findById(postId);
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
+
     }
 
 
