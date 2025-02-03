d273bb484173ee27b523b223744302ae2e38c484
pigglehyun <sarah0661@pusan.ac.kr>
2025-02-03 18:55:53 +0900

    feat: create / delete post

--- src/main/java/com/culture/BAEUNDAY/domain/post/PostService.java
+++ src/main/java/com/culture/BAEUNDAY/domain/post/PostService.java
@@ -1,13 +1,13 @@
 package com.culture.BAEUNDAY.domain.post;
 
-
+import com.culture.BAEUNDAY.domain.post.DTO.PostRequest;
 import com.culture.BAEUNDAY.domain.post.DTO.PostResponse;
 import com.culture.BAEUNDAY.utils.CursorRequest;
 import com.culture.BAEUNDAY.utils.CursorResponse;
 import com.culture.BAEUNDAY.utils.PageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.function.Function;
@@ -18,27 +18,65 @@ public class PostService {
 
     private final PostJPARepository postJPARepository;
     private static final int PAGE_SIZE_PLUS_ONE = 5 + 1;
+
+    @Transactional
     public PageResponse<? extends Comparable<?> , PostResponse.FindAllDTO> findAll(String sort, String status, String fee, String cursor, Long cursorId) {
         List<Post> posts;
 
         switch( sort ) {
             case "id" -> {
-                CursorRequest<Long> page = new CursorRequest(PAGE_SIZE_PLUS_ONE, cursor, Long.class, cursorId );
+                CursorRequest<Long> page = new CursorRequest<>(PAGE_SIZE_PLUS_ONE, cursor, Long.class, cursorId );
                 posts = postJPARepository.findAllByIdLessThanCursor(page.cursor, page.request);
                 return createCursorPageResponse(Post::getId, posts);
             }
             case "heart" -> {
-                CursorRequest<Long> page = new CursorRequest(PAGE_SIZE_PLUS_ONE, cursor, Long.class, cursorId );
+                CursorRequest<Long> page = new CursorRequest<>(PAGE_SIZE_PLUS_ONE, cursor, Long.class, cursorId );
                 posts = postJPARepository.findAllByHeartLessThanCursor(page.cursor, page.cursorID, page.request);
                 return createCursorPageResponse(Post::getNumsOfHeart, posts);
             }
             case "recent" -> {
-                CursorRequest<LocalDateTime> page = new CursorRequest(PAGE_SIZE_PLUS_ONE, cursor, LocalDateTime.class, cursorId );
+                CursorRequest<LocalDateTime> page = new CursorRequest<>(PAGE_SIZE_PLUS_ONE, cursor, LocalDateTime.class, cursorId );
                 posts = postJPARepository.findAllByDateLessThanCursor( page.cursor, page.cursorID, page.request);
                 return createCursorPageResponse(Post::getCreatedDate, posts);
             }
             default -> throw new IllegalArgumentException();
         }
+    }
+
+    @Transactional
+    public void create(PostRequest.PostRequestDto request) {
+//        User user = userService.find()
+        Long user_id = 1L;
+
+        Post post = Post.builder()
+                .title(request.title())
+                .imgURL(request.imgURL())
+                .subject(request.subject())
+                .goal(request.goal())
+                .outline(request.outline())
+                .targetStudent(request.targetStudent())
+                .level(request.level())
+                .contactMethod(request.contactMethod())
+                .fee(request.fee())
+                .startDate(request.startDate())
+                .endDate(request.endDate())
+                .province(request.province())
+                .city(request.city())
+                .address(request.address())
+                .minP(request.minimumPeople())
+                .maxP(request.maximumPeople())
+                .content(request.content())
+                .status(request.status())
+                .createdDate(request.createdDate())
+                .deadline(request.deadLine())
+                .user_id(user_id) // TODO : User 매핑
+                .build();
+        postJPARepository.save(post);
+    }
+
+    public void delete(Long postId) {
+        Post post = getPostById(postId);
+        postJPARepository.delete(post); //TODO: user ?
     }
 
     private <T extends Comparable<T>> PageResponse<T, PostResponse.FindAllDTO> createCursorPageResponse(Function<Post, T> cursorExtractor, List<Post> posts){
@@ -62,7 +100,6 @@ public class PostService {
         Long nextCursorId = lastPost.getId();
 
         return new PageResponse<>(new CursorResponse<>(hasNext, size, nextCursor, nextCursorId), findAllDTO);
-
     }
 
     public PageResponse<?, PostResponse.FindByIdDTO> findById(Long postId) {
@@ -75,4 +112,6 @@ public class PostService {
     public Post getPostById(Long postId) {
         return postJPARepository.findById(postId).orElseThrow(IllegalArgumentException::new);
     }
+
+
 }
