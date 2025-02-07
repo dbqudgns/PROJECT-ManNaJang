9194f17bfaa20ec310a8dd56573869fad81458c0
pigglehyun <sarah0661@pusan.ac.kr>
2025-02-07 23:03:33 +0900

    feat: add Fee Range Enum

---
+++ src/main/java/com/culture/BAEUNDAY/domain/post/Fee.java
@@ -0,0 +1,24 @@
+package com.culture.BAEUNDAY.domain.post;
+
+public enum Fee {
+
+    FREE(0,0),
+    UNDER_3(0,29999),
+    BETWEEN3_5(30000,49999),
+    BETWEEN5_10(50000,99999),
+    OVER_10(99999,Integer.MAX_VALUE);
+
+    private Integer min;
+    private Integer max;
+
+    Fee(int i, int i1) {
+    }
+
+    public Integer getMin(){ return min; }
+    public Integer getMax(){ return max; }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
