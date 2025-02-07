3bb18485d75e0e32d30a0b6c1f7a6a52e3b964c3
pigglehyun <sarah0661@pusan.ac.kr>
2025-02-07 23:06:31 +0900

    add findAllPost logic

--- src/main/java/com/culture/BAEUNDAY/domain/post/PostRestController.java
+++ src/main/java/com/culture/BAEUNDAY/domain/post/PostRestController.java
@@ -3,26 +3,30 @@ package com.culture.BAEUNDAY.domain.post;
 
 import com.culture.BAEUNDAY.domain.post.DTO.PostRequest;
 import com.culture.BAEUNDAY.domain.post.DTO.PostResponse;
+import com.culture.BAEUNDAY.jwt.Custom.CustomUserDetails;
 import com.culture.BAEUNDAY.utils.ApiUtils;
 import com.culture.BAEUNDAY.utils.PageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
+@Tag(name = "포스트 api", description = "포스트 관련 기능")
 public class PostRestController {
 
     private final PostService postService;
 
-
+    @Operation(summary = "모든 프로그램 조회 ( 홈 ) sort = id/heart/recent , status = ING/AVAILBLE/END , fee = FREE/UNDER_3/BETWEEN3_5/BETWEEN5_10/OVER_10 ")
     @GetMapping
-    public ResponseEntity<?> findAllPost(@RequestParam(value = "sort") String sort,
-                                         @RequestParam(value = "status") String status,
-                                         @RequestParam(value = "fee") String fee,
+    public ResponseEntity<?> findAllPost(@RequestParam(value = "sort", defaultValue = "id") String sort,
+                                         @RequestParam(value = "status", required = false) Status status,
+                                         @RequestParam(value = "fee", required = false) Fee fee,
                                          @RequestParam(value = "cursor", required = false) String cursor,
                                          @RequestParam(value = "cursorId", required = false) Long cursorId ) {
 
@@ -30,38 +34,37 @@ public class PostRestController {
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
+    @Operation(summary = "특정 프로그램 조회")
     @GetMapping("/{postId}")
     public  ResponseEntity<?> findPostById(@PathVariable(name = "postId") Long postId ){
         PageResponse<?, PostResponse.FindByIdDTO> responseDTO = postService.findById(postId);
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
 
     }
-
+    @Operation(summary = "프로그램 생성")
     @PostMapping
-    public ResponseEntity<?> create(
-//            @AuthenticationPrincipal UserPrincipal userPrincipal,
-                                    @RequestBody @Valid PostRequest.PostRequestDto request,
-                                    Errors errors){
-        postService.create(request);
+    public ResponseEntity<?> create(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                    @RequestBody @Valid PostRequest.PostRequestDto request
+    ){
+        postService.create(userDetails.getUsername(), request);
         return ResponseEntity.ok().body(ApiUtils.success("프로그램 생성 완료"));
     }
 
-
+    @Operation(summary = "프로그램 수정")
     @PutMapping("/{postId}")
-    public ResponseEntity<?> update(
-//            @AuthenticationPrincipal UserPrincipal userPrincipal,
-            @PathVariable(name = "postId") Long postId,
-            @RequestBody PostRequest.PostRequestDto request
+    public ResponseEntity<?> update(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                    @PathVariable(name = "postId") Long postId,
+                                    @RequestBody PostRequest.PostRequestDto request
     ){
-        postService.update(postId, request);
+        postService.update(userDetails.getUsername(), postId, request);
         return ResponseEntity.ok().body(ApiUtils.success("프로그램 수정 완료"));
     }
+    @Operation(summary = "프로그램 삭제")
     @DeleteMapping("/{postId}")
-    public ResponseEntity<?> delete(
-//            @AuthenticationPrincipal UserPrincipal userPrincipal,
-            @PathVariable(name = "postId") Long postId
+    public ResponseEntity<?> delete(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                    @PathVariable(name = "postId") Long postId
             ){
-        postService.delete(postId);
+        postService.delete(userDetails.getUsername(), postId);
         return ResponseEntity.ok().body(ApiUtils.success("프로그램 삭제 완료"));
     }
 
