3bc24fe5530f6bcb599ec86c33fe87fd32eb5aee
pigglehyun <sarah0661@pusan.ac.kr>
2025-02-08 19:08:58 +0900

    feat: add heart logic

--- src/main/java/com/culture/BAEUNDAY/domain/heart/Heart.java
+++ src/main/java/com/culture/BAEUNDAY/domain/heart/Heart.java
@@ -1,9 +1,20 @@
 package com.culture.BAEUNDAY.domain.heart;
 
+import com.culture.BAEUNDAY.domain.post.Post;
 import com.culture.BAEUNDAY.domain.user.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Builder
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "heart_tb")
 public class Heart {
 
@@ -16,10 +27,13 @@ public class Heart {
     @JoinColumn(name = "user_id")
     private User user;
 
-    /*
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
-    */
+
+    @Column(nullable = false)
+    private LocalDateTime createdDate;
+
 
 }
