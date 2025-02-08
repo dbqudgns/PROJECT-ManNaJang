a978858fa42fe13bee8038e466254b4952645bc7
pigglehyun <sarah0661@pusan.ac.kr>
2025-02-08 14:26:14 +0900

    add cursor pagination to review

--- src/main/java/com/culture/BAEUNDAY/domain/review/ReviewRepository.java
+++ src/main/java/com/culture/BAEUNDAY/domain/review/ReviewRepository.java
@@ -1,5 +1,6 @@
 package com.culture.BAEUNDAY.domain.review;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,7 +9,7 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @Query("select r from Review r where r.reviewee.id = :userId")
-    List<Review> findByRevieweeId(@Param("userId") Long id);
+    @Query("select r from Review r where r.reviewee.id = :userId and r.id < :cursor order by r.id desc ")
+    List<Review> findByRevieweeIdWithCursor(@Param("userId") Long id, @Param("cursor") Long cursor, Pageable request);
 
 }
